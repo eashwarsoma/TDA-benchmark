1cb39b820fab2ed68551f76178b2bcc41ca373b2
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-10-19 19:38:02 -0400

    Cleaned up code

--- bench.R
+++ bench.R
@@ -1,15 +1,32 @@
-bench <- function(pointdata, whichTDA, featdim, iter) { #point data input is required. program that is calculated is based off text string. Dimensional features and iteration number for benchmark should also be specified
-  if (whichTDA == "stats") { 
-    time <- mark(calculate_homology(pointdata, dim = featdim), iterations = iter)
+# NB: maxscale = 5 is used in TDA package examples, so used here (no default)
+
+# point data input is required. program that is calculated is based off text string
+# dimensional features and iteration number for benchmark should also be specified
+bench <- function(dataset, which.TDA, feature.dim, iter) {
+  # TDAstats
+  if (whichTDA == "stats") {
+    time <- mark(calculate_homology(pointdata, dim = featdim),
+                 iterations = iter)
+  # TDA - Dionysus
+  } else if (whichTDA == "Dionysus") {
+    time <- mark(ripsDiag(pointdata, maxdimension = featdim, maxscale = 5,
+                          location = FALSE, library = "Dionysus"),
+                 iterations = iter)
+  # TDA - GUDHI
+  } else if (whichTDA == "GUDHI") {
+    time <- mark(ripsDiag(pointdata, maxdimension = featdim, maxscale = 5,
+                          location = FALSE, library = "GUDHI"),
+                 iterations = iter)
+  # TDA - GUDHI (alpha complex)
+  } else if (whichTDA == "GUDHIalpha") {
+    time <- mark(alphaComplexDiag(pointdata, maxdimension = featdim,
+                                  location = FALSE, library = "GUDHI"),
+                 iterations = iter)
+  # none of the above
+  } else {
+    stop("Invalid TDA engine selected")
   }
-  if (whichTDA == "Dionysus") {
-    time <- mark(ripsDiag(pointdata, maxdimension = featdim, maxscale = 5, location = FALSE, library = "Dionysus"), iterations = iter)
-  }
-  if (whichTDA == "GUDHI") {
-    time <- mark(ripsDiag(pointdata, maxdimension = featdim, maxscale = 5, location = FALSE, library = "GUDHI"), iterations = iter)
-  }
-  if (whichTDA == "GUDHIalpha") {
-    time <- mark(alphaComplexDiag(pointdata, maxdimension = featdim, location = FALSE, library = "GUDHI"), iterations = iter)
-  }
-  return(time[1,3]) #row 1 column 3 is specifically median bench time
+  
+  # row 1 column 3 is specifically median bench time
+  return(time[1,3])
 }
