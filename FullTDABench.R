b7f0a7a13df009e285dbc032c08dc4db79125710
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-10-19 19:47:31 -0400

    Formatted code

--- FullTDABench.R
+++ FullTDABench.R
@@ -8,42 +8,47 @@ library(pryr)
 
 source("generate-data.R")
 source("bench.R")
-source("MemorySize.R")
-
+source("MemorySize.R") # need to review
 
 ###FIX THIS LATER ON###
 #need if-then statements to stop function if (for example)
 #  feature.dim > data.dimension
 #make more user-friendly
-TDA_bench <- function(measure, data.type, data.dimensions, num.points, feature.dimensions, TDA.library, num.iteration, ...) { #step 1, generate the pointdata, step 2 benchmark
+TDA_bench <- function(measure, data.type, data.dimensions, num.points,
+                      feature.dimensions, TDA.library, num.iteration) {
+  #step 1, generate the dataset
   if (data.type == "circle") {
     pointdata <- unifcircle(num.points, data.dimensions)
-  }
-  if (data.type == "uniform") {
+  } else if (data.type == "uniform") {
     pointdata <- unifbox(num.points, data.dimensions)
-  }
-  if (data.type == "annulus") {
+  } else if (data.type == "annulus") {
     pointdata <- noisycircle(num.points, data.dimensions)
-  }
-  if (data.type == "torus") {
+  } else if (data.type == "torus") {
     pointdata <- torus(num.points)
+  } else {
+    stop("Invalid data type")
   }
   
+  # step 2 benchmark
   if (measure == "time") {
-    exec.time <- bench(pointdata, TDA.library, feature.dimensions, num.iteration)
+    exec.time <- bench(pointdata, TDA.library,
+                       feature.dimensions, num.iteration)
     exec.time <- exec.time[[1,1]]
     return(exec.time)
   }
   if (measure == "memory") {
-    memory <- memory(pointdata, TDA.library, feature.dimensions)
-    return(memory)
+    mem.data <- memory(pointdata, TDA.library,
+                       feature.dimensions)
+    return(mem.data)
   }
 }
 
-test1 <- TDA_bench(measure = "time", data.type = "uniform", data.dimensions = 3, num.points = 150, feature.dimensions = 1, TDA.library = "GUDHIalpha", num.iteration = 11)
-test2 <- TDA_bench(measure = "time", data.type = "uniform", data.dimensions = 3, num.points = 150, feature.dimensions = 1, TDA.library = "stats", num.iteration = 11)
+test1 <- TDA_bench(measure = "time", data.type = "uniform",
+                   data.dimensions = 3, num.points = 150,
+                   feature.dimensions = 1, TDA.library = "GUDHIalpha",
+                   num.iteration = 11)
 
-
-
-
-
+test2 <- TDA_bench(measure = "time", data.type = "uniform",
+                   data.dimensions = 3, num.points = 150,
+                   feature.dimensions = 1, TDA.library = "stats",
+                   num.iteration = 11)
