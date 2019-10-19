f7479d8cd36b5f0c776ade1976dc289c094db817
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-10-19 19:13:05 -0400

    cleaned up UnifBox

--- UnifBox.R
+++ UnifBox.R
@@ -1,9 +1,13 @@
+# generates an n-dimension box (0, 1) of uniformly distributed points
 unifbox <- function(box.points, box.dimensions) {
-  list <- data.frame() 
-  for (i in 1:box.dimensions) { #adds a column of randomaly generated points 0 to 1 for how many ever dimensions specified
-    col <- runif (box.points, 0, 1) #number of points is based off function input
-    list[1:box.points, i] <- cbind(col)
+  # empty 
+  to.calc.hom <- matrix(NA, nrow = box.points, ncol = box.dimensions)
+  
+  # adds a column of randomaly generated points 0 to 1 for how many ever dimensions specified
+  for (i in 1:box.dimensions) {
+    col <- runif(box.points, 0, 1)
+    to.calc.hom[, i] <- col
   }
-  to.calc.hom <- data.matrix(list, rownames.force = NA)
+  
   return(to.calc.hom)
 }
