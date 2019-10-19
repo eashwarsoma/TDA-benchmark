8206a272d1dd9322f8b4ee4ff9bac3d6cf2febcc
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-10-19 18:22:05 -0400

    Adjusted 4-d data generation

--- UnifCircle.R
+++ UnifCircle.R
@@ -38,26 +38,37 @@ unifcircle <- function(circle.points, circle.dimensions) {
   }
   
   if (circle.dimensions == 4) { #follows same principle as previous but with more parameters
-    list.parameters <- data.frame() 
-    repeat {
+    # each loop generates one row of data
+    for (curr_row in 1:circle.points) {
+      
+      # generate valid w, x, y, z
       x <- runif(1, -1, 1)
       y <- runif(1, -1, 1)
       z <- runif(1, -1, 1)
       w <- runif(1, -1, 1)
-      if (x^2 + y^2 < 1 & z^2 + w^2 < 1 ) {
-        list.parameters[nrow(list.parameters)+1,1:4] <- rbind(x,y,z,w)
+      while (x ^ 2 + y ^ 2 >= 1 |
+             w ^ 2 + z ^ 2 >= 1) {
+        w <- runif(1, -1, 1)
+        x <- runif(1, -1, 1)
+        y <- runif(1, -1, 1)
+        z <- runif(1, -1, 1)
       }
-      if (nrow(list.parameters) == circle.points) {
-        break
-      }
+      
+      # generate coordinates of sphere
+      temp <- sqrt((1 - x ^ 2 - y ^ 2) / (w ^ 2 + z ^ 2))
+      x1 <- x
+      x2 <- y
+      x3 <- z * temp
+      x4 <- w * temp
+      
+      # store into data frame
+      to.calc.hom[curr_row, ] <- rbind(x1, x2, x3, x4)
     }
-    x1 <- select(list.parameters, 1)
-    x2 <- select(list.parameters, 2)
-    x3 <- select(list.parameters, 3)
-    x4 <- select(list.parameters, 4)
-    list.unifcircle <- data.frame()
-    list.unifcircle <- cbind(x1, x2, x3*sqrt((1-x1^2-x2^2)/(x3^2+x4^2)), x4*sqrt((1-x1^2-x2^2)/(x3^2+x4^2)))
+    
+    # cast df into matrix
+    to.calc.hom <- as.matrix(to.calc.hom)
   }
-  #to.calc.hom <- data.matrix(list.unifcircle, rownames.force = NA) #data is converted from data frame to matrix
-  return(to.calc.hom) #every function will create this matrix
+  
+  # return answer variable
+  return(to.calc.hom)
 }
