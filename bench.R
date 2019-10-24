372e7353058037be413fa203944b7d12fd611a19
Eashwar Somasundaram <Soma@Eashwars-MacBook-Pro.local>
2019-10-23 20:01:29 -0400

    Changed variable names to be consistent across all R scripts (e.g. changed all instances of whichTDA to TDA.libary)

--- bench.R
+++ bench.R
@@ -2,26 +2,26 @@
 
 # point data input is required. program that is calculated is based off text string
 # dimensional features and iteration number for benchmark should also be specified
-bench <- function(pointdata, whichTDA, featdim, iter) {
+bench <- function(pointdata, TDA.library, featdim, num.iterations) {
   # TDAstats
-  if (whichTDA == "stats") {
+  if (TDA.library == "stats") {
     time <- mark(calculate_homology(pointdata, dim = featdim),
-                 iterations = iter)
+                 iterations = num.iterations)
   # TDA - Dionysus
-  } else if (whichTDA == "Dionysus") {
+  } else if (TDA.library == "Dionysus") {
     time <- mark(ripsDiag(pointdata, maxdimension = featdim, maxscale = 5,
                           location = FALSE, library = "Dionysus"),
-                 iterations = iter)
+                 iterations = num.iterations)
   # TDA - GUDHI
-  } else if (whichTDA == "GUDHI") {
+  } else if (TDA.library == "GUDHI") {
     time <- mark(ripsDiag(pointdata, maxdimension = featdim, maxscale = 5,
                           location = FALSE, library = "GUDHI"),
-                 iterations = iter)
+                 iterations = num.iterations)
   # TDA - GUDHI (alpha complex)
-  } else if (whichTDA == "GUDHIalpha") {
+  } else if (TDA.library == "GUDHIalpha") {
     time <- mark(alphaComplexDiag(pointdata, maxdimension = featdim,
                                   location = FALSE, library = "GUDHI"),
-                 iterations = iter)
+                 iterations = num.iterations)
   # none of the above
   } else {
     stop("Invalid TDA engine selected")
