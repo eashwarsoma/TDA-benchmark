9d527335d497b67e758818cc9c889191c10a1dfb
Eashwar Somasundaram <evs27@case.edu>
2019-11-04 15:55:06 -0500

    Added a function where it would check already collected data and automatically remove them from the set of parameters needed to benchmark

--- TDABenchTimeNew.R
+++ TDABenchTimeNew.R
@@ -64,14 +64,14 @@ vars.circle <- as_tibble(expand.grid(measure = "time", data.type = "circle",
                                      data.dimensions = 2:4, num.points = seq(50, 500, 25),
                                      feature.dimensions = 1:3, 
                                      TDA.library = c("stats", "Dionysus", "GUDHI", "GUDHIalpha"),
-                                     num.iteration = 1, file.name = "time.csv")) %>% subset(feature.dimensions < data.dimensions)
+                                     num.iteration = 1, file.name = "time1.csv")) %>% subset(feature.dimensions < data.dimensions)
 
 #Making Annulus Variables for time
 vars.noisycircle <- as_tibble(expand.grid(measure = "time", data.type = "annulus",
                                           data.dimensions = 2:4, num.points = seq(50, 500, 25),
                                           feature.dimensions = 1:3, 
                                           TDA.library = c("stats", "Dionysus", "GUDHI", "GUDHIalpha"),
-                                          num.iteration = 1, file.name = "time.csv")) %>% subset(feature.dimensions < data.dimensions)
+                                          num.iteration = 1, file.name = "time1.csv")) %>% subset(feature.dimensions < data.dimensions)
 
 
 #Making Box Variables for time
@@ -79,14 +79,14 @@ vars.box <- as_tibble(expand.grid(measure = "time", data.type = "uniform",
                                   data.dimensions = 2:5, num.points = seq(50, 500, 25),
                                   feature.dimensions = 1:4, 
                                   TDA.library = c("stats", "Dionysus", "GUDHI", "GUDHIalpha"),
-                                  num.iteration = 1, file.name = "time.csv")) %>% subset(feature.dimensions < data.dimensions)
+                                  num.iteration = 1, file.name = "time1.csv")) %>% subset(feature.dimensions < data.dimensions)
 
 #Making Torus Variables for time
 vars.torus <- as_tibble(expand.grid(measure = "time", data.type = "torus",
                                     data.dimensions = 3, num.points = seq(50, 500, 25),
                                     feature.dimensions = 1:2, 
                                     TDA.library = c("stats", "Dionysus", "GUDHI", "GUDHIalpha"),
-                                    num.iteration = 1, file.name = "time.csv")) %>% subset(feature.dimensions < data.dimensions)
+                                    num.iteration = 1, file.name = "time1.csv")) %>% subset(feature.dimensions < data.dimensions)
 ####
 
 ####Assemble variables for time...and delete the ones my laptop can't handle####
@@ -167,64 +167,22 @@ vars.all <- vars.all[!(vars.all$data.type == "uniform" & vars.all$num.points>70
 vars.all <- vars.all[!(vars.all$data.dimensions > 3 
                        & vars.all$TDA.library == "GUDHIalpha"), ]
 
-####
+#Reads in collected data so far
+vars.tested <- read.csv("time1.csv")
+colnames(vars.tested) <- c("measure", "data.type", "data.dimensions",
+                           "num.points", "feature.dimensions", "TDA.library", "time")
 
-####splitting table into 10 tables to manage for my computer and running code####
-#Comment out this next session for the real deal
-vars.all.1 <- vars.all[1:160, ]
-vars.all.2 <- vars.all[161:320, ]
-vars.all.3 <- vars.all[321:480, ]
-vars.all.4 <- vars.all[481:640, ]
-vars.all.5 <- vars.all[641:800, ]
-vars.all.6 <- vars.all[801:960, ]
-vars.all.7 <- vars.all[961:1120, ]
-vars.all.8 <- vars.all[1121:1280, ]
-vars.all.9 <- vars.all[1281:1399, ]
+#From the variables, removes what has already been collected from each session
+vars.all <- vars.all %>% anti_join(vars.tested)
 
-mapply(TDA_bench, vars.all.4$measure, vars.all.4$data.type,
-       vars.all.4$data.dimensions, vars.all.4$num.points,
-       vars.all.4$feature.dimensions, vars.all.4$TDA.library,
-       vars.all.4$num.iteration, vars.all.4$file.name)
+mapply(TDA_bench, vars.all$measure, vars.all$data.type,
+       vars.all$data.dimensions, vars.all$num.points,
+       vars.all$feature.dimensions, vars.all$TDA.library,
+       vars.all$num.iteration, vars.all$file.name)
 
-mapply(TDA_bench, vars.all.3$measure, vars.all.3$data.type,
-       vars.all.3$data.dimensions, vars.all.3$num.points,
-       vars.all.3$feature.dimensions, vars.all.3$TDA.library,
-       vars.all.3$num.iteration, vars.all.3$file.name)
+print("done")
 
-mapply(TDA_bench, vars.all.1$measure, vars.all.1$data.type,
-       vars.all.1$data.dimensions, vars.all.1$num.points,
-       vars.all.1$feature.dimensions, vars.all.1$TDA.library,
-       vars.all.1$num.iteration, vars.all.1$file.name)
 
-mapply(TDA_bench, vars.all.2$measure, vars.all.2$data.type,
-       vars.all.2$data.dimensions, vars.all.2$num.points,
-       vars.all.2$feature.dimensions, vars.all.2$TDA.library,
-       vars.all.2$num.iteration, vars.all.2$file.name)
-
-mapply(TDA_bench, vars.all.5$measure, vars.all.5$data.type,
-       vars.all.5$data.dimensions, vars.all.5$num.points,
-       vars.all.5$feature.dimensions, vars.all.5$TDA.library,
-       vars.all.5$num.iteration, vars.all.5$file.name)
-
-mapply(TDA_bench, vars.all.6$measure, vars.all.6$data.type,
-       vars.all.6$data.dimensions, vars.all.6$num.points,
-       vars.all.6$feature.dimensions, vars.all.6$TDA.library,
-       vars.all.6$num.iteration, vars.all.6$file.name)
-
-mapply(TDA_bench, vars.all.7$measure, vars.all.7$data.type,
-       vars.all.7$data.dimensions, vars.all.7$num.points,
-       vars.all.7$feature.dimensions, vars.all.7$TDA.library,
-       vars.all.7$num.iteration, vars.all.7$file.name)
-
-mapply(TDA_bench, vars.all.8$measure, vars.all.8$data.type,
-       vars.all.8$data.dimensions, vars.all.8$num.points,
-       vars.all.8$feature.dimensions, vars.all.8$TDA.library,
-       vars.all.8$num.iteration, vars.all.8$file.name)
-
-mapply(TDA_bench, vars.all.9$measure, vars.all.9$data.type,
-       vars.all.9$data.dimensions, vars.all.9$num.points,
-       vars.all.9$feature.dimensions, vars.all.9$TDA.library,
-       vars.all.9$num.iteration, vars.all.9$file.name)
 
 ####
 
