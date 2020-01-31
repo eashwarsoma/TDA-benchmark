f5a1097b38365bcf1e62b266d725f7cb145b26bb
Eashwar Somasundaram <evs27@case.edu>
2020-01-31 06:13:04 -0500

    Corrected figures so that shape and color change together. Modified figure legends slightly

--- Figures/Final_Figures_Reproducible.R
+++ Figures/Final_Figures_Reproducible.R
@@ -28,7 +28,8 @@ data.time$max.time <- apply(data.time[,7:16],1,max)
 #Create Factor labels for libraries
 data.time$library <- factor(data.time$library,
                             levels = c("stats","Dionysus","GUDHI", "GUDHIalpha"),
-                            labels = c("TDAstats", "Dionysus", "GUDHI Rips", "GUDHI Alpha"))
+                            labels = c("TDAstats (Ripser)", 
+                                       "TDA (Dionysus)", "TDA (GUDHI Rips)", "TDA (GUDHI Alpha)"))
 
 #Read in object size data
 data.mem <- read.csv("./mem1.csv", header = FALSE)
@@ -40,7 +41,8 @@ colnames(data.mem) <- c("measure.type", "point.cloud", "point.cloud.dim",
 #Create Factor labels for libraries
 data.mem$library <- factor(data.mem$library,
                            levels = c("stats","Dionysus","GUDHI", "GUDHIalpha"),
-                           labels = c("TDAstats", "Dionysus", "GUDHI Rips", "GUDHI Alpha"))
+                           labels = c("TDAstats (Ripser)", 
+                                      "TDA (Dionysus)", "TDA (GUDHI Rips)", "TDA (GUDHI Alpha)"))
 
 
 ####Figure_1####
@@ -48,15 +50,16 @@ data.mem$library <- factor(data.mem$library,
 
 #Selected torus data scanning for two dimensional features
 data.fig.1 <- subset(data.time, point.cloud == "torus" & feat.dim == 2 & 
-                       library != "GUDHI Alpha") 
+                       library != "TDA (GUDHI Alpha)") 
 
 #Point graph; X: Num.points; Y: Runtime; Color: TDA library
-fig.1 <- ggplot(data.fig.1, aes(x=num.points, y=avg.time, color=library)) + 
+fig.1 <- ggplot(data.fig.1, aes(x=num.points, y=avg.time, color=library, shape=library)) + 
   geom_point()+
   geom_errorbar(data.fig.1, mapping = aes(x=num.points, 
                                           ymin=avg.time - std, 
                                           ymax=avg.time + std)) +
   labs(color = "TDA Library",
+       shape = "TDA Library",
        x = "Number of Points on Torus",
        y = "Average Run Time",
        title = "Rips Complex Run Times on 3D Torus Point Clouds",
@@ -70,7 +73,7 @@ fig.1 <- fig.1 + theme_classic() +
         plot.title = element_text(hjust = 0.5),
         axis.line = element_line(colour = "black"),
         legend.text.align = 0,
-        legend.title.align = 0)
+        legend.title.align = 0) 
 
 #Saving plot as png for later editing
 ggsave("./Figures/Unrasterized_Images/fig1.png", plot = fig.1,
@@ -84,17 +87,18 @@ ggsave("./Figures/Unrasterized_Images/fig1.png", plot = fig.1,
 #across R packages for n-dimensional sphere (n = 2, 3, 4)
 
 #Selected all boxes up to n-1 dimensional features
-data.fig.2 <- subset(data.time, point.cloud == "circle" & library != "GUDHI Alpha")
+data.fig.2 <- subset(data.time, point.cloud == "circle" & library != "TDA (GUDHI Alpha)")
 data.fig.2 <- data.fig.2[(data.fig.2$point.cloud.dim - 1 == data.fig.2$feat.dim), ]
 
 #Point graph facet; X: Num.points; Y: Runtime; Color: TDA library; Facet: Dimensions of Sphere
-fig.2 <- ggplot(data.fig.2, aes(x=num.points, y=avg.time, color=library)) + 
+fig.2 <- ggplot(data.fig.2, aes(x=num.points, y=avg.time, color=library, shape=library)) + 
   geom_point() +
   facet_wrap( ~ point.cloud.dim, ncol=3, scales = "free") + 
   geom_errorbar(data.fig.2, mapping = aes(x=num.points, 
                                           ymin=avg.time - std, 
                                           ymax=avg.time + std)) +
   labs(color = "TDA Library",
+       shape = "TDA Library",
        x = "Number of Points on N-Sphere",
        y = "Average Run Time",
        title = "Rips Complex Run Times For n-Dimensional Spheres",
@@ -125,17 +129,18 @@ ggsave("./Figures/Unrasterized_Images/fig2.png", plot = fig.2,
 ####Figure 3: Runtime of persistent homology calculation as a function of feature dimension####
 #Select all annulus data for all dimension point cloud and n-1 dim features
 data.fig.3 <- subset(data.time, point.cloud == "uniform" & 
-                       point.cloud.dim == 8 & library != "GUDHI Alpha" & 
+                       point.cloud.dim == 8 & library != "TDA (GUDHI Alpha)" & 
                        (num.points == 10 | num.points == 15| num.points == 20))
 data.fig.3$num.points <- as.factor(data.fig.3$num.points)
 
 #Point graph facet; X: Num.points; Y: Runtime; Color: TDA library; Facet: Dimensions of Annulus
-fig.3 <- ggplot(data.fig.3, aes(x=feat.dim, y=avg.time, color=num.points)) + 
+fig.3 <- ggplot(data.fig.3, aes(x=feat.dim, y=avg.time, color=num.points, shape=num.points)) + 
   geom_point() + facet_wrap(~library, scales = "free") + 
   geom_errorbar(data.fig.3, mapping = aes(x=feat.dim, 
                                           ymin=avg.time - std, 
                                           ymax=avg.time + std)) +
   labs(color = "Number of \nPoints",
+       shape = "Number of \nPoints",
        x = "Feature Dimensions",
        y = "Average Run Time",
        title = "Run Times For Extracting Dimensional \nFeatures on an 8 Dimensional Box",
@@ -165,17 +170,18 @@ ggsave("./Figures/Unrasterized_Images/fig3.png", plot = fig.3,
 
 #Select all 3D annuluses and change feature dimension to a factor variable
 data.fig.4 <- subset(data.time, point.cloud == "annulus" & feat.dim == 1 & 
-                       (library == "GUDHI Rips" | library == "GUDHI Alpha"))
+                       (library == "TDA (GUDHI Rips)" | library == "TDA (GUDHI Alpha)"))
 data.fig.4$point.cloud.dim <- as.factor(data.fig.4$point.cloud.dim)
 data.fig.4 <- as.data.frame(data.fig.4)
 
 #Point graph facet; X: Num.points; Y: Runtime; Color: point.cloud dim; Facet: library
-fig.4 <- ggplot(data.fig.4, aes(x=num.points, y=avg.time, color=point.cloud.dim)) + 
+fig.4 <- ggplot(data.fig.4, aes(x=num.points, y=avg.time, color=point.cloud.dim, shape=point.cloud.dim)) + 
   geom_point() + facet_wrap(~library, scales = "free") + 
   geom_errorbar(data.fig.4, mapping = aes(x=num.points, 
                                           ymin=avg.time - std, 
                                           ymax=avg.time + std)) +
   labs(color = "Annulus \nDimension",
+       shape = "Annulus \nDimension",
        x = "Number of Points",
        y = "Average Run Time",
        title = "Rips vs Alpha Complex Run Times For Extracting \n1 Dimensional Features on N-dimensional Annuluses",
@@ -205,9 +211,10 @@ ggsave("./Figures/Unrasterized_Images/fig4.png", plot = fig.4,
 data.fig.5a <- subset(data.mem, point.cloud.dim == 3 & feat.dim == 2)
 
 #Point graph facet; X: Num.points; Y: Memory; Color: Library; Facet: Pointcloud
-fig.5a <- ggplot(data.fig.5a, aes(x=num.points, y=memory, color=library)) + 
+fig.5a <- ggplot(data.fig.5a, aes(x=num.points, y=memory, color=library, shape=library)) + 
   geom_point() + facet_wrap(~point.cloud, scales = "free") + 
   labs(color = "Complex",
+       shape = "Complex",
        x = "Number of Points",
        y = "Object Size",
        title = "Object Size of Boundary Matrix",
@@ -217,7 +224,8 @@ fig.5a <- ggplot(data.fig.5a, aes(x=num.points, y=memory, color=library)) +
 
 #Editing the colors (Making everything blank)
 fig.5a <- fig.5a + theme_classic() +
-  theme(legend.position = c(.915, 0.915),
+  theme(legend.position = c(.9, 0.8),
+        legend.spacing.x = unit(.005, 'cm'),
         legend.title = element_text(size = 9),
         legend.text = element_text(size = 7),
         plot.title = element_text(hjust = 0.5),
@@ -227,20 +235,21 @@ fig.5a <- fig.5a + theme_classic() +
         strip.background = element_blank(),
         #plot.margin = unit(c(1,3,1,1), "cm"),
         strip.text.x = element_blank())
-
+fig.5a
 #Saving plot as png for later editing
 ggsave("./Figures/Unrasterized_Images/fig5a.png", plot = fig.5a,
        scale = 1, width = 6, height = 4, units = "in",
        dpi = 400, limitsize = TRUE)
 
 #Show what Rips Object Size depends on
-data.fig.5b <- subset(data.mem, library == "GUDHI Rips")
+data.fig.5b <- subset(data.mem, library == "TDA (GUDHI Rips)")
 data.fig.5b$feat.dim <- as.factor(data.fig.5b$feat.dim)
 
 #Point graph facet; X: Num.points; Y: Memory; Color: Feat.Dim; 
-fig.5b <- ggplot(data.fig.5b, aes(x=num.points, y=memory, color=feat.dim)) + 
+fig.5b <- ggplot(data.fig.5b, aes(x=num.points, y=memory, color=feat.dim, shape=feat.dim)) + 
   geom_point() + 
-  labs(color = "Feature \n Dimension",
+  labs(color = "Feature \nDimension",
+       shape = "Feature \nDimension",
        x = "Number of Points",
        y = "Object Size",
        title = "Object Size of Rips Complex Boundary Matrix",
@@ -268,7 +277,7 @@ ggsave("./Figures/Unrasterized_Images/fig5b.png", plot = fig.5b,
        dpi = 400, limitsize = TRUE)
 
 #Show what Alpha Complex Object Size depends on
-data.fig.5c <- subset(data.mem, library == "GUDHI Alpha")
+data.fig.5c <- subset(data.mem, library == "TDA (GUDHI Alpha)")
 #This is necessary since Alpha Complex Object Size Function Did Not
 #Calculate Feat Dimensions lower than Point Cloud Dim - 1 in all cases
 #This subset is necessary to remove the extraneous conditions that
@@ -279,9 +288,10 @@ data.fig.5c$feat.dim <- as.factor(data.fig.5c$feat.dim)
 
 
 #Point graph facet; X: Num.points; Y: Memory; Color: Feat.Dim; 
-fig.5c <- ggplot(data.fig.5c, aes(x=num.points, y=memory, color=feat.dim)) + 
+fig.5c <- ggplot(data.fig.5c, aes(x=num.points, y=memory, color=feat.dim, shape=feat.dim)) + 
   geom_point() + facet_wrap(~point.cloud, scales = "free") +
-  labs(color = "Feature \n Dimension",
+  labs(color = "Feature \nDimension",
+       shape = "Feature \nDimension",
        x = "Number of Points",
        y = "Object Size",
        title = "Object Size of Alpha Complex Boundary Matrix",
@@ -292,7 +302,7 @@ fig.5c <- ggplot(data.fig.5c, aes(x=num.points, y=memory, color=feat.dim)) +
 
 #Editing the colors (Making everything blank)
 fig.5c <- fig.5c + theme_classic() + theme(
-  legend.position = c(.915, 0.915),
+  legend.position = c(.915, 0.945),
   legend.title = element_text(size = 9),
   legend.text = element_text(size = 7),
   plot.title = element_text(hjust = 0.5),
@@ -312,16 +322,19 @@ ggsave("./Figures/Unrasterized_Images/fig5c.png", plot = fig.5c,
 
 
 #####Figure 6 (for discussion) TDAstats vs GUDHIalpha for runtime.####
+#Removing point cloud less than 20 since these points were specifically for multidimension box
 data.fig.6 <- subset(data.time, point.cloud.dim == 3 & feat.dim == 2 & 
-                       (library == "TDAstats" | library == "GUDHI Alpha"))
+                       (library == "TDAstats (Ripser)" | library == "TDA (GUDHI Alpha)")) %>%
+              subset(num.points >= 20)
 
 #Point graph facet; X: Num.points; Y: time; Color: Library; Facet: Pointcloud
-fig.6 <- ggplot(data.fig.6, aes(x=num.points, y=avg.time, color=library)) + 
+fig.6 <- ggplot(data.fig.6, aes(x=num.points, y=avg.time, color=library, shape=library)) + 
   facet_wrap(~point.cloud, scales = "free") + 
   geom_errorbar(data.fig.6, mapping = aes(x=num.points, 
                                           ymin=avg.time - std, 
                                           ymax=avg.time + std)) + geom_point() +
   labs(color = "Library",
+       shape = "Library",
        x = "Number of Points",
        y = "Average Run Time (s)",
        title = "Rips (TDAstats) vs Alpha Complex (GUDHI) \nRun Times on 3D Point Clouds",
@@ -332,12 +345,13 @@ fig.6 <- ggplot(data.fig.6, aes(x=num.points, y=avg.time, color=library)) +
 #Editing the colors (Making everything blank)
 fig.6 <- fig.6 + theme_classic()
 fig.6 <- fig.6 + theme(
-  legend.position = c(.915, 1.12),
+  legend.position = c(.13, .18),
   legend.title = element_text(size = 9),
   legend.text = element_text(size = 7),
   plot.title = element_text(hjust = 0.5),
   legend.text.align = 0,
-  legend.title.align = 0.9,
+  legend.title.align = 0,
+  legend.background=element_blank(),
   #plot.title = element_text(hjust = 0.5),
   axis.text.x = element_text(angle=0, hjust=.025),
   strip.background = element_blank(),
